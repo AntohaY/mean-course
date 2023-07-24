6a237e4e843cb5edfecc0109e7bd72f3c69167d0
AntohaY <antonyakovenko2401@gmail.com>
2023-07-24 23:36:13 +0300

    refactor: :tada: removed module file to use SCAMs instead

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -8,3 +10,15 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'mean-course';
 }
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
