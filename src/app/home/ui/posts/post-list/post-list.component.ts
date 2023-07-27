926f2648220b74ca6b8dd69bc696baeb77aa461b
AntohaY <antonyakovenko2401@gmail.com>
2023-07-28 02:16:22 +0300

    feat: added edit and delete buttons, no functionality

--- src/app/home/ui/posts/post-list/post-list.component.ts
+++ src/app/home/ui/posts/post-list/post-list.component.ts
@@ -2,12 +2,13 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { MatExpansionModule } from '@angular/material/expansion';
 import { PostsService } from 'src/app/home/data-access/posts.service';
+import { MatButtonModule } from '@angular/material/button';
 @Component({
     selector: 'app-post-list',
     templateUrl: 'post-list.component.html',
     styleUrls: ['post-list.component.scss'],
     standalone: true,
-    imports: [MatExpansionModule, CommonModule]
+    imports: [MatExpansionModule, CommonModule, MatButtonModule]
 })
 
 export class PostListComponent implements OnInit {
